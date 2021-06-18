414a4c992f7311e922a16ef6919a918d350e9f79
EmmanuelQC <60098681+EmmanuelQC@users.noreply.github.com>
2021-06-18 11:36:08 +0800

    Testing Server Update

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return "Hello, World"
 
-
+'''
 @app.route('/calculated', methods=["GET"])
 def calculate():
     return render_template("calculated.html", grades=request.args.get("grades"))
+'''
 
 
 if __name__ == '__main__':
