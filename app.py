592863c48ca17b6ed35e8f383693a00960a54434
EmmanuelQC <60098681+EmmanuelQC@users.noreply.github.com>
2021-06-18 12:04:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    # return render_template("index.html")
-    return "Hello, World"
+    return render_template("index.html")
+    # return "Hello, World"
 
 '''
 @app.route('/calculated', methods=["GET"])
